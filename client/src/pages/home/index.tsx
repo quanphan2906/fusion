ac548160ac68eba5c7332beb2a994e8a991edce0
Quan Phan <hoangquanphan2906@gmail.com>
2024-06-23 11:04:16 -0700

    fix bug

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,34 +1,35 @@
-import React from 'react';
-import Split from 'react-split';
-import TabsComponent from '@/components/TabComponent';
-import Placeholder from '@/components/PlaceHolder';
-import 'tailwindcss/tailwind.css'; // Ensure Tailwind CSS is imported
+import React, { useState } from "react";
+import Split from "react-split";
+import TabsComponent from "@/components/TabComponent";
+import Placeholder from "@/components/PlaceHolder";
+import "tailwindcss/tailwind.css"; // Ensure Tailwind CSS is imported
 
 const Home: React.FC = () => {
+	const [suggestions, setSuggestions] = useState<any[]>([]);
 
-  return (
-    <div className="h-screen">
-      <Split
-        sizes={[70, 30]}
-        minSize={100}
-        expandToMin={false}
-        gutterSize={5} // Increase the gutter size
-        gutterAlign="center"
-        snapOffset={30}
-        dragInterval={1}
-        direction="horizontal"
-        cursor="col-resize"
-        className="flex w-full h-full"
-      >
-        <div className="w-full h-full p-4">
-          <TabsComponent />
-        </div>
-        <div className="w-full h-full fixed top-0 right-0">
-          <Placeholder />
-        </div>
-      </Split>
-    </div>
-  );
-}
+	return (
+		<div className="h-screen">
+			<Split
+				sizes={[70, 30]}
+				minSize={100}
+				expandToMin={false}
+				gutterSize={5} // Increase the gutter size
+				gutterAlign="center"
+				snapOffset={30}
+				dragInterval={1}
+				direction="horizontal"
+				cursor="col-resize"
+				className="flex w-full h-full"
+			>
+				<div className="w-full h-full p-4">
+					<TabsComponent setSuggestions={setSuggestions} />
+				</div>
+				<div className="w-full h-full fixed top-0 right-0">
+					<Placeholder suggestions={suggestions} />
+				</div>
+			</Split>
+		</div>
+	);
+};
 
 export default Home;
